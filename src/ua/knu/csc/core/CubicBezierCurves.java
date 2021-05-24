a4b619fc22cb1e8cc80b1a0cfd10d8f7f1677e90
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-05-25 01:13:09 +0300

    Implement the calculateCubicBezierCurvesCoefficients() method

--- src/ua/knu/csc/core/CubicBezierCurves.java
+++ src/ua/knu/csc/core/CubicBezierCurves.java
@@ -6,11 +6,76 @@ import java.awt.Point;
 public class CubicBezierCurves {
     private final ArrayList<Point> points;
 
+    private double[] aX;
+    private double[] aY;
+
+    private double[] bX;
+    private double[] bY;
+
     public CubicBezierCurves(ArrayList<Point> points) {
         if (points == null) {
             throw new NullPointerException("The specified argument 'points' is null.");
         }
         this.points = points;
+    }
+
+    public void calculateCubicBezierCurvesCoefficients() {
+        int n = points.size() - 1;
+
+        double[][] cX = new double[n][n + 1];
+        double[][] cY = new double[n][n + 1];
+
+        for (int i = 1; i < (n - 1); i++) {
+            cX[i][i - 1] = 1.0d;
+            cX[i][i] = 4.0d;
+            cX[i][i + 1] = 1.0d;
+        }
+
+        cX[0][0] = 2.0d;
+        cX[0][1] = 1.0d;
+
+        cX[n - 1][n - 2] = 2.0d;
+        cX[n - 1][n - 1] = 7.0d;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cY[i][j] = cX[i][j];
+            }
+        }
+
+        for (int i = 1; i < (n - 1); i++) {
+            cX[i][n] = 2.0d * (2.0d * points.get(i).getX() + points.get(i + 1).getX());
+            cY[i][n] = 2.0d * (2.0d * points.get(i).getY() + points.get(i + 1).getY());
+        }
+
+        cX[0][n] = points.get(0).getX() + 2.0d * points.get(1).getX();
+        cX[n - 1][n] = 8.0d * points.get(n - 1).getX() + points.get(n).getX();
+
+        cY[0][n] = points.get(0).getY() + 2.0d * points.get(1).getY();
+        cY[n - 1][n] = 8.0d * points.get(n - 1).getY() + points.get(n).getY();
+
+        double[] aX = new double[n];
+        double[] aY = new double[n];
+
+        Utils.gaussianElimination(cX, n, n + 1, aX);
+        Utils.gaussianElimination(cY, n, n + 1, aY);
+
+        double[] bX = new double[n];
+        double[] bY = new double[n];
+
+        for (int i = 0; i < (n - 1); i++) {
+            bX[i] = 2.0d * points.get(i + 1).getX() - aX[i + 1];
+            bY[i] = 2.0d * points.get(i + 1).getY() - aY[i + 1];
+        }
+
+        bX[n - 1] = (aX[n - 1] + points.get(n).getX()) / 2.0d;
+        bY[n - 1] = (aY[n - 1] + points.get(n).getY()) / 2.0d;
+
+        this.aX = aX;
+        this.aY = aY;
+
+        this.bX = bX;
+        this.bY = bY;
     }
 
     public static double calculateCubicBezierCurveValue(double a, double b, double c, double d, double t) {
